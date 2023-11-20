a454fe5174879878287c69148f49a0878374cb2d
Junaid Hassan <junaid.hassan@evampsaanga.com>
2023-11-20 16:10:17 +0500

    example code added

--- lib/pin_code_rules_utils_juni1289.dart
+++ lib/pin_code_rules_utils_juni1289.dart
@@ -1,7 +1,46 @@
-library pin_code_rules_utils_juni1289;
+class PinCodeRulesHelperUtil {
+  /// private constructor
+  PinCodeRulesHelperUtil._();
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+  /// the one and only instance of this singleton
+  static final instance = PinCodeRulesHelperUtil._();
+
+  ///PIN Code fields Rule#1
+  ///check that if the PIN code has all same characters
+  ///[pinCode] is the mandatory param to check the pinCode Rule#1
+  bool isPinCodeHasSameChars({required String pinCode}) {
+    bool isSameChars = true;
+    if (pinCode.isNotEmpty) {
+      for (int i = 0; i < pinCode.length; i++) {
+        if (pinCode[i] != pinCode[0]) {
+          isSameChars = false;
+        }
+      }
+    } else {
+      isSameChars = true;
+    }
+
+    return isSameChars;
+  }
+
+  ///PIN Code Fields Rule#2
+  ///check that if the PIN code is a palindrome string
+  ///[pinCode] is the mandatory param to check the pinCode Rule#2
+  bool isPinCodePalindrome({required String pinCode}) {
+    bool isPinCodePalindrome = true;
+    if (pinCode.isNotEmpty) {
+      String stringToReverse = pinCode;
+      String reversedString = "";
+      String beforeReverse = stringToReverse;
+      for (int j = stringToReverse.length - 1; j >= 0; j--) {
+        reversedString += stringToReverse[j];
+      }
+      if (beforeReverse == reversedString) {
+        isPinCodePalindrome = true;
+      } else {
+        isPinCodePalindrome = false;
+      }
+    }
+    return isPinCodePalindrome;
+  }
 }
--- test/pin_code_rules_utils_juni1289_test.dart
+++ test/pin_code_rules_utils_juni1289_test.dart
@@ -1,12 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:pin_code_rules_utils_juni1289/pin_code_rules_utils_juni1289.dart';
-
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
-}
+void main() {}
